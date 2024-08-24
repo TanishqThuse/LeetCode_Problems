79094b53a0883aa803788173ccc11e9d34e841be
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-08-24 22:01:29 +0530

    Update file 24/08/2024, 22:01:28

---
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.length();
+
+        map<char , int> map;
+        for(int i=0; i<n; i++){
+            map[s[i]]++;
+        }
+
+        for(int i=0; i<n; i++){
+            if(map[s[i]] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
