9e9a2ed809a16484cf85071f9f8197913d60493c
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-08-28 19:46:55 +0530

    Create file

---
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+}
