a2c44ab0cfe2f54f5aa3d7a7d6545cf5ff5c9c76
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-09-06 07:28:45 +0530

    Update file 9/6/2024, 7:28:46 AM

--- 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.java
+++ 3217-DeleteNodesFromLinkedListPresentinArray/3217-DeleteNodesFromLinkedListPresentinArray.java
@@ -1,69 +1,30 @@
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        Set<Integer> numSet = new HashSet<>();
+        // Put array in HashSet for O(1) access
+        HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
-            numSet.add(num);
+            set.add(num);
         }
 
+        // Dummy node to handle edge cases
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode start = head;
-        ListNode prev = dummy;
+        ListNode front = head;
+        ListNode back = dummy;
 
-        while (start != null) {
-            if (numSet.contains(start.val)) {
-                prev.next = start.next;
-            } else {
-                prev = start;
+        while (front != null) {
+            if (set.contains(front.val)) {
+                // Remove the node
+                back.next = front.next;
             }
-            start = start.next;
+            else {
+                // Move the back pointer only if we don't remove the node
+                back = front;
+            }
+            // Move the front pointer forward
+            front = back.next;
         }
 
         return dummy.next;
     }
 }
-
-
-
-
-//Unnecedsary com[iocated]
-// /**
-//  * Definition for singly-linked list.
-//  * public class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode() {}
-//  *     ListNode(int val) { this.val = val; }
-//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//  * }
-//  */
-// class Solution {
-//     public boolean isPresent(int val, int nums[]){
-//         for(int i=0; i<nums.length; i++){
-//             if(nums[i]==val){
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-
-//     public ListNode modifiedList(int[] nums, ListNode head) {
-//         ListNode dummy = new ListNode(0);
-//         dummy.next = head;
-//         ListNode start = head;
-//         ListNode prev = dummy;
-//         while(start!=null){
-//             if(isPresent(start.val, nums)){
-//                 prev.next = start.next;
-//                 start=start.next;
-//                 continue;
-//             //  0,1
-//             //  0->2 
-//             //  
-//             }
-//             prev = start;
-//             start = start.next;
-//         }
-//         return dummy.next;
-//     }
-// }
