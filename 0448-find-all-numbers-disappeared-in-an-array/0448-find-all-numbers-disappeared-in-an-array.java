d50824a685287da6bbd590ba8c8dbc62cfb47504
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-10-17 07:15:24 +0530

    Time: 20 ms (11.96%), Space: 55.2 MB (31.56%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,23 +1,48 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            map.put(i+1, 0);
-        }
+        Set<Integer> set = new HashSet<>();
 
-        //now simply put it from array
-        for(int i=0; i<n; i++){
-            map.put(nums[i], map.get(nums[i])+1);
+        // Add all numbers from the array to the set
+        for (int i = 0; i < n; i++) {
+            set.add(nums[i]);
         }
 
         List<Integer> numbersWhoWentToBuyMilk = new ArrayList<>();
 
-        for(int i=1; i<=n; i++){
-            if(map.get(i) == 0){
+        // Find missing numbers by checking if they're not in the set
+        for (int i = 1; i <= n; i++) {
+            if (!set.contains(i)) {
                 numbersWhoWentToBuyMilk.add(i);
             }
         }
+
         return numbersWhoWentToBuyMilk;
     }
 }
+
+
+//Using hashset bad time complexity
+// class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         int n = nums.length;
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int i=0; i<n; i++){
+//             map.put(i+1, 0);
+//         }
+
+//         //now simply put it from array
+//         for(int i=0; i<n; i++){
+//             map.put(nums[i], map.get(nums[i])+1);
+//         }
+
+//         List<Integer> numbersWhoWentToBuyMilk = new ArrayList<>();
+
+//         for(int i=1; i<=n; i++){
+//             if(map.get(i) == 0){
+//                 numbersWhoWentToBuyMilk.add(i);
+//             }
+//         }
+//         return numbersWhoWentToBuyMilk;
+//     }
+// }
