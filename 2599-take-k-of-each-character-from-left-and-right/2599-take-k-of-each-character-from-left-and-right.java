2a6195325518e1993491b0e35d7ed7d029464ab9
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-11-20 12:41:22 +0530

    Time: 16 ms (31.82%), Space: 45.2 MB (75.15%) - LeetHub

---
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int takeCharacters(String s, int k) {
+        // Total counts
+        int[] count = new int[3];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        
+        
+        if (Math.min(Math.min(count[0], count[1]), count[2]) < k) {
+            return -1;
+        }
+        
+        // Sliding Window
+        int res = Integer.MAX_VALUE;
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            count[s.charAt(r) - 'a']--;
+            
+            while (Math.min(Math.min(count[0], count[1]), count[2]) < k) {
+                count[s.charAt(l) - 'a']++;
+                l++;
+            }
+            res = Math.min(res, s.length() - (r - l + 1));
+        }
+        return res;
+    }
+}
+
+// class Solution {
+//     public int takeCharacters(String s, int k) {
+//         // HashSet<Integer> set = new HashSet<>();
+//         HashMap<Character ,Integer> map = new HashMap<>();
+//         int n = s.length();
+//         for(int i = 0 ; i<n; i++){
+
+//         }
+//     }
+// }
