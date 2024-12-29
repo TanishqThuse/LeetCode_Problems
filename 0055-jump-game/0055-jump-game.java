9685508f508a250a06cc215b6c6fbb77afcef236
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2024-12-29 20:02:45 +0530

    0055-JUMP-GAME Stats: Time: 3 ms (46.94%), Space: 45.5 MB (82.64%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,31 @@
-//This program looks like it could be solved by Greedy and DP
+// //This program looks like it could be solved by Greedy and DP
 
-//Greedy solution
+// //Greedy solution
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         int n = nums.length;
+//         int maxJumpIndex= 0;
+//         for(int i=0; i<n; i++){
+//             if(i<=maxJumpIndex){
+//                 int jumping = i+nums[i];
+//                 maxJumpIndex = Math.max(maxJumpIndex, jumping );
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+//Approach - 2 : 
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int maxJumpIndex= 0;
-        for(int i=0; i<n; i++){
-            if(i<=maxJumpIndex){
-                int jumping = i+nums[i];
-                maxJumpIndex = Math.max(maxJumpIndex, jumping );
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
     }
 }
