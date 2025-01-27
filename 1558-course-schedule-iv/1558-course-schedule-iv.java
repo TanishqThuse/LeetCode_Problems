33922624c6701be44ae9810afbd112babfa10c88
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2025-01-27 21:34:44 +0530

    Time: 409 ms (10%), Space: 51.2 MB (5.44%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    // Performs DFS and returns true if there's a path between src and target
+    // and false otherwise.
+    private boolean isPrerequisite(
+        Map<Integer, List<Integer>> adjList,
+        boolean[] visited,
+        int src,
+        int target
+    ) {
+        visited[src] = true;
+
+        if (src == target) {
+            return true;
+        }
+
+        boolean answer = false;
+        List<Integer> neighbors = adjList.getOrDefault(src, new ArrayList<>());
+        for (int adj : neighbors) {
+            if (!visited[adj]) {
+                answer =
+                    answer || isPrerequisite(adjList, visited, adj, target);
+            }
+        }
+        return answer;
+    }
+
+    public List<Boolean> checkIfPrerequisite(
+        int numCourses,
+        int[][] prerequisites,
+        int[][] queries
+    ) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+        for (int[] edge : prerequisites) {
+            adjList
+                .computeIfAbsent(edge[0], k -> new ArrayList<>())
+                .add(edge[1]);
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i < queries.length; i++) {
+            // Reset the visited array for each query
+            boolean[] visited = new boolean[numCourses];
+            result.add(
+                isPrerequisite(adjList, visited, queries[i][0], queries[i][1])
+            );
+        }
+
+        return result;
+    }
+}
