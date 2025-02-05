36f84ad9bf177b95099066617c281c0c679703b2
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2025-02-05 13:59:47 +0530

    Time: 1 ms (99.44%), Space: 57.5 MB (13.39%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,19 +1,33 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-
-        dp[0] = nums[0];
-
-        int ans = dp[0];
-
-        for(int i=1; i<n; i++){
-            dp[i] = Math.max(nums[i], nums[i]+dp[i-1]);
-            //since we have two choices either take current num or take currentNum + prevSum ==> SO we take max among both
-
-            ans = Math.max(ans, dp[i]);
+        int sum=nums[0];
+        int maxi = nums[0];
+        int i=1;
+        while(i<nums.length){
+            sum = Math.max(nums[i],nums[i]+sum);
+            maxi = Math.max(sum,maxi);
+            i++;
         }
-
-        return ans;
+        return maxi;
     }
 }
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+
+//         dp[0] = nums[0];
+
+//         int ans = dp[0];
+
+//         for(int i=1; i<n; i++){
+//             dp[i] = Math.max(nums[i], nums[i]+dp[i-1]);
+//             //since we have two choices either take current num or take currentNum + prevSum ==> SO we take max among both
+
+//             ans = Math.max(ans, dp[i]);
+//         }
+
+//         return ans;
+//     }
+// }
