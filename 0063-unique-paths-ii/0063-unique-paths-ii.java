09c58441c23dadda38a431f5b6462d7e635aa64e
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2025-06-10 18:05:03 +0530

    Time: 0 ms (100%), Space: 41.9 MB (39.72%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        // Tabulation based approach
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int dp[][] = new int[m][n];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n ;j++){
+                if(obstacleGrid[i][j] == 1){
+                    // obstacle found
+                    dp[i][j] = 0;
+                }
+                else if(i==0 && j==0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0){
+                        up = dp[i-1][j];
+                    }
+                    if(j>0){
+                        left = dp[i][j-1];
+                    }
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
