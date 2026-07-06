d21afdc5d332a54c59a4883450739e1bb3a19f16
Tanishq Thuse <147257309+TanishqThuse@users.noreply.github.com>
2026-07-06 23:43:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.24%) - LeetHub

---
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string binary(int n){
+        string s = "";
+    while (n > 0) {
+        s += (n % 2) + '0';
+        n /= 2;
+    }
+    return s;
+    }
+
+    bool consecutiveSetBits(int n) {
+        // convert n to binary
+        string s = binary(n);
+
+        int cnt = 0;
+        for(int i=1; i<s.size(); i++){
+            if((s[i] == s[i-1]) && s[i] == '1'){
+                cnt++;
+            }
+        }
+
+        return cnt==1;
+    }
+};
